df9ac69c76de2fe797a200ac4a8392dad6d0e581
Percy <percival.ngan@gmail.com>
2019-12-16 18:09:24 -0600

    Created NetworkDataLoader protocol

---
+++ Astronomy/Model/NetworkDataLoader.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkDataLoader.swift
+//  Astronomy
+//
+//  Created by Percy Ngan on 12/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+protocol NetworkDataLoader {
+
+	func loadData(from request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void)
+
+	func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void)
+}

2ee8d79f66f78ec8efc9b75b19fb6e33d38e2f13
Percy <percival.ngan@gmail.com>
2019-12-16 23:56:46 -0600

    Added fetchMarsRover and fetchPhotos to test

---
+++ AstronomyTests/MarsRoverClientTests.swift
@@ -0,0 +1,20 @@
+//
+//  MarsRoverClientTests.swift
+//  MarsRoverClientTests
+//
+//  Created by Percy Ngan on 12/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class MarsRoverClientTests: XCTestCase {
+
+	func testFetchMarsRover() {
+
+	}
+
+	fetchFetchPhotos() {
+		
+	}
+}
